ceafbbd85796513b4ae0b339320df012ee04fa9a
Coocker <coockergamer@gmail.com>
2017-04-13 18:06:05 +0200

    Update help.lua

--- ZHelp/help.lua
+++ ZHelp/help.lua
@@ -1,7 +1,8 @@
+if
 TriggerEvent('es:addCommand', 'help', function(source, args, user)
     TriggerClientEvent("chatMessage", source, "", {0, 0, 0}, "/cuff ID - Menotter un joueur")
     TriggerClientEvent("chatMessage", source, "", {0, 0, 0}, "/heal ID - Réanimer un joueur")
     TriggerClientEvent("chatMessage", source, "", {0, 0, 0}, "M - Contrôles du véhicule")
     TriggerClientEvent("chatMessage", source, "", {0, 0, 0}, "X - Mettre les mains en l'air")
     TriggerClientEvent("chatMessage", source, "", {0, 0, 0}, "F5 - Menu personnel")
-end)
+end
